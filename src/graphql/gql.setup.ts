ecf8dc47e6ca6625fc371ff45f99ff2fa0c34e6d
Xuan Tho <tholxph31196@fpt.edu.vn>
2024-07-11 23:00:11 +0700

    url

--- src/graphql/gql.setup.ts
+++ src/graphql/gql.setup.ts
@@ -6,20 +6,18 @@ import {
 } from "@apollo/client";
 import Cookies from "js-cookie";
 const httpLink = createHttpLink({
-  uri: process.env.NEXT_PUBLIC_SERVER_URI,
+  uri: "https://odyssey-lift-off-part5-server-production.up.railway.app/", // change this to YOUR server URI
 });
 
-const authMiddleWare = new ApolloLink(
-  (operation, forward) => {
-    operation.setContext({
-      headers: {
-        accesstoken: Cookies.get("access_token"),
-        refreshtoken: Cookies.get("refresh_token"),
-      },
-    });
-    return forward(operation);
-  }
-);
+const authMiddleWare = new ApolloLink((operation, forward) => {
+  operation.setContext({
+    headers: {
+      accesstoken: Cookies.get("access_token"),
+      refreshtoken: Cookies.get("refresh_token"),
+    },
+  });
+  return forward(operation);
+});
 export const graphqlClient = new ApolloClient({
   link: authMiddleWare.concat(httpLink),
   cache: new InMemoryCache(),
